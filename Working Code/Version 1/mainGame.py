de0260014b56cfe4aa3c72566c252c5d4b1571d6
KyrumX <kyrumx@hotmail.nl>
2017-01-19 13:27:56 +0100

    Fixed some bugs: - Screen scaling caused an odd resolution - Character sprites are now correct
    Added:
    - Movement finalized

--- Working Code/Version 1/mainGame.py
+++ Working Code/Version 1/mainGame.py
@@ -1,5 +1,8 @@
 """Copryright 2017, Project 2 Groep 1"""
 
+import ctypes
+
+
 import pygame
 from pygame.locals import *
 import time
@@ -19,32 +22,34 @@ class Player:
 
 
 def program(maxp):
+    # Fix voor het voorkomen van stretchen wat je resolutie verpest
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
     width = 1920
     height = 1080
     size = (width, height)
-    background = pygame.image.load("Afbeeldingen/gamebackground.png")
+    background = pygame.image.load("Afbeeldingen/background.png")
     img1 = pygame.image.load("Afbeeldingen/SP1.png")
     img2 = pygame.image.load("Afbeeldingen/SP2.png")
     img3 = pygame.image.load("Afbeeldingen/SP3.png")
     img4 = pygame.image.load("Afbeeldingen/SP4.png")
 
-    pygame.init()
 
     screen = pygame.display.set_mode(size, pygame.FULLSCREEN)
     font = pygame.font.Font(None, 30)
 
     if maxp <= 2:
-        playerOne = Player(550, 875, img1)
-        playerTwo = Player(550, 875, img2)
+        playerOne = Player(680, 1010, img1)
+        playerTwo = Player(857, 1010, img2)
     elif maxp == 3:
-        playerOne = Player(550, 875, img1)
-        playerTwo = Player(550, 875, img2)
-        playerThree = Player(550, 875, img3)
+        playerOne = Player(680, 1010, img1)
+        playerTwo = Player(851, 1010, img2)
+        playerThree = Player(1036, 1010, img3)
     else:
-        playerOne = Player(550, 875, img1)
-        playerTwo = Player(550, 875, img2)
-        playerThree = Player(550, 875, img3)
-        playerFour = Player(550, 875, img4)
+        playerOne = Player(680, 1010, img1)
+        playerTwo = Player(851, 1010, img2)
+        playerThree = Player(1036, 1010, img3)
+        playerFour = Player(1213, 1010, img4)
     ant = "1999"
     cp = 1
 
@@ -63,6 +68,8 @@ def program(maxp):
                 # if playerAnt == ant:
                 if cp == 1:
                     playerOne.update()
+                    # if playerOne.y == 5:
+                    #     mainloop = False
                     cp += 1
                 elif cp == 2:
                     playerTwo.update()
@@ -94,5 +101,5 @@ def program(maxp):
             playerThree.draw(screen)
             playerFour.draw(screen)
         clock.tick(60)
-        pygame.display.flip()
+        pygame.display.update()
 
