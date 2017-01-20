d0f13aaecadfec610d7bf9968ccd0d998f0a5c19
KyrumX <kyrumx@hotmail.nl>
2017-01-20 09:30:58 +0100

    Added dice throw

--- Aarons files - NOT FINAL/mainGame.py
+++ Aarons files - NOT FINAL/mainGame.py
@@ -1,16 +1,20 @@
 """Copryright 2017, Project 2 Groep 1"""
 
 import ctypes
-
-
 import pygame
 from pygame.locals import *
 import time
+import random
+
 clock = pygame.time.Clock()
 # Fix voor het voorkomen van stretchen wat je resolutie verpest
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware()
 
+def diceThrow():
+    gooi = [1, 2, 3]
+    throw = random.choice(gooi)
+    return throw
 
 class Player:
     def __init__(self, x, y, image):
@@ -18,8 +22,8 @@ class Player:
         self.y = y
         self.image = image
 
-    def update(self):
-        self.y -= 67
+    def update(self, cg):
+        self.y -= (67 * cg)
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
@@ -57,35 +61,36 @@ def program(maxp):
 
     mainloop = True
     while mainloop:
+        mouse = pygame.mouse.get_pos()
         k = pygame.key.get_pressed()
         if k[K_ESCAPE]:
             mainloop = False
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 mainloop = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                print("Spatie")
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1486 and mouse[0] < 1736 and mouse[1] > 422 and mouse[1] < 651:
+                cg = diceThrow()
                 # playerAnt = input("Antwoord")
                 # if playerAnt == ant:
                 if cp == 1:
-                    playerOne.update()
+                    playerOne.update(cg)
                     # if playerOne.y == 5:
                     #     mainloop = False
                     cp += 1
                 elif cp == 2:
-                    playerTwo.update()
+                    playerTwo.update(cg)
                     if maxp > 2:
                         cp += 1
                     else:
                         cp -= 1
                 elif cp == 3:
-                    playerThree.update()
+                    playerThree.update(cg)
                     if maxp > 3:
                         cp += 1
                     else:
                         cp -= 2
                 elif cp == 4:
-                    playerFour.update()
+                    playerFour.update(cg)
                     cp -= 3
         print(cp)
         screen.blit(background, (0,0))
