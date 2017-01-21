eb032ae169de07c64915ed97c27a1f0283fe0afa
Selim <selimaydi@gmail.com>
2017-01-21 18:33:24 +0100

    Update database.py
    added login function

--- Working Code/Version 1/database.py
+++ Working Code/Version 1/database.py
@@ -41,3 +41,16 @@ def download_scores():
 def download_top_score():
     result = interact_with_database("SELECT * FROM score ORDER BY score")[0][1]
     return result
+
+def login():
+    usr = input('Voer je gebruikersnaam in') # momenteel via console
+    pw = input('Voer je wachtwoord in')
+
+    find = interact_with_database("SELECT * FROM users WHERE username = '{}' and password = '{}'".format(usr, pw))
+
+    if interact_with_database("SELECT * FROM users WHERE username = '{}'".format(usr)) == []:
+        print("De ingevoerde gebruikersnaam bestaat niet")
+    elif interact_with_database("SELECT * FROM users WHERE username = '{}'".format(usr)) != [] and find == []:
+        print("De ingevoerde wachtwoord is verkeerd")
+    else:
+        print("Je bent ingelogd!")
