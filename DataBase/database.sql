05fe8972ac295d800b57664e705fb758db667664
PikaPire <Markstout1998@gmail.com>
2017-01-25 11:32:27 +0100

    Queries for database(verbeterd)

--- DataBase/database.sql
+++ DataBase/database.sql
@@ -1,11 +1,12 @@
 CREATE TABLE QnA(
-	Question_id 		int UNIQUE Primary key,
+	Question_id 		int Primary key,
     Question_type 		Varchar(25),
+	Question_Catagory	Varchar(30),
     Question 			varchar(250),
-    Awnser1				varchar(200),
-    Awnser2				varchar(200),
-    Awnser3				varchar(200),
-    Correct_awnser		varchar(200)
+    Anwser1				varchar(200),
+    Anwser2				varchar(200),
+    Anwser3				varchar(200),
+    Correct_anwser		varchar(200)
 );
 
 CREATE TABLE Score(
