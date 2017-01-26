b96b64e7113f18340319dc2fb2e6591f57d13476
Selim <selimaydi@gmail.com>
2017-01-26 14:26:04 +0100

    Update mainMenu.py

--- Working Code/Version 1/mainMenu.py
+++ Working Code/Version 1/mainMenu.py
@@ -19,17 +19,10 @@ resolution = (1920, 1080)
 screen = pygame.display.set_mode(resolution, pygame.FULLSCREEN)
 pygame.display.set_caption("Euromast Spel")
 
-myfont = pygame.font.SysFont('Comic Sans MS', 70)
-myfont2 = pygame.font.SysFont('Comic Sans MS', 100)
-label = myfont.render('Spelen', True, black)
-label2 = myfont.render('Spel sluiten', True, white)
-label4 = myfont.render('Regels', True, black)
-label3 = myfont2.render('Het Euromast spel', True, white)
-playgametext = myfont.render('Hier wordt het spel gespeeld!', True, white)
-
 
 class Button:
-    def __init__(self, msg, fontsize, x, y, rectanglecolor=green, textcolor=black, h=100, w=600): # Optionele waardes hier zijn rectanglecolor, textcolor en de dimensies van de rectangle
+    def __init__(self, msg, fontsize, x, y, rectanglecolor=green, textcolor=black, h=100,
+                 w=600):  # Optionele waardes hier zijn rectanglecolor, textcolor en de dimensies van de rectangle
         self.msg = msg
         self.fonttype = "Comic Sans MS"
         self.textcolor = textcolor
@@ -40,7 +33,8 @@ class Button:
         self.w = w
         self.h = h
 
-    def button(self, scalehelper=40): # de button functie zorgt ervoor dat de button getekend word met gecentreerde text
+    def button(self,
+               scalehelper=40):  # de button functie zorgt ervoor dat de button getekend word met gecentreerde text
         design = pygame.draw.rect(screen, self.rectanglecolor, [self.x, self.y, self.w, self.h])
         myfont = pygame.font.SysFont(self.fonttype, self.fontsize)
         label = myfont.render(self.msg, True, self.textcolor)
@@ -55,8 +49,10 @@ class Button:
         finalx = xcalc - div
         labelpos = (finalx, y_is)
         screen.blit(label, labelpos)
+
     def press(self):
         self.rectanglecolor = black
+
 
 def drawtext(msg, fontsize, textcolor, x, y):
     myfont = pygame.font.SysFont("Comic Sans MS", fontsize)
@@ -64,16 +60,19 @@ def drawtext(msg, fontsize, textcolor, x, y):
     labelpos = (x, y)
     screen.blit(label, labelpos)
 
+
 def button(msg, x, y, w, h, ic, ac, action=None):
     mouse = pygame.mouse.get_ps()
     click = pygame.mouse.get_pressed()
     print(click)
+
+
 prachtig = (66, 170, 244)
 screen.fill(prachtig)
 menu = True
 clicked = False
 rules = False
-
+terug = False
 
 while True:
     mouse = pygame.mouse.get_pos()
@@ -83,23 +82,30 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1244 and mouse[0] < 1712 and mouse[1] > 349 and mouse[1] < 470 and menu == True:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 0 and mouse[0] < 400 and mouse[
+            1] > 0 and mouse[1] < 150 and menu == True:
             clicked = True
             menu = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1244 and mouse[0] < 1712 and mouse[1] > 720 and mouse[1] < 840 and menu == True:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1516 and mouse[0] < 1920 and \
+                        mouse[1] > 200 and mouse[1] < 350 and menu == True:
             rules = True
             menu = False
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1245 and mouse[0] < 1711 and mouse[1] > 534 and mouse[1] < 657 and menu == True:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 30 and mouse[0] < 300 and mouse[
+            1] > 1000 and mouse[1] < 1080 and rules == True:
+            terug = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1516 and mouse[0] < 1920 and \
+                        mouse[1] > 0 and mouse[1] < 150 and menu == True:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1384 and mouse[0] < 1461 and mouse[1] > 617 and mouse[1] < 686 and menu == False:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 175 and mouse[0] < 540 and mouse[
+            1] > 700 and mouse[1] < 830 and menu == False:
             program(2)
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1534 and mouse[0] < 1609 and mouse[1] > 617 and mouse[1] < 686 and menu == False:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 770 and mouse[0] < 1130 and \
+                        mouse[1] > 700 and mouse[1] < 830 and menu == False:
             program(3)
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1684 and mouse[0] < 1761 and mouse[1] > 617 and mouse[1] < 688 and menu == False:
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1360 and mouse[0] < 1770 and \
+                        mouse[1] > 700 and mouse[1] < 830 and menu == False:
             program(4)
-
-
 
     k = pygame.key.get_pressed()
     if k[K_ESCAPE]:
@@ -107,38 +113,24 @@ while True:
         sys.exit()
 
     if menu == True:
-        background = pygame.image.load('Afbeeldingen/euro.jpg')
+        background = pygame.image.load('Afbeeldingen/euromastspelbgwtext.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
-        bg = pygame.image.load('Afbeeldingen/menubg.png')
-        screen.blit(bg, (1230, 200))
-        if clicked == False:
-            spelen = pygame.image.load('Afbeeldingen/spelen.png')
-            screen.blit(spelen, (1245, 350))
-            afsluiten = pygame.image.load('Afbeeldingen/afsluiten.png')
-            screen.blit(afsluiten, (1245, 534))
-            regels = pygame.image.load('Afbeeldingen/regels.png')
-            screen.blit(regels, (1245, 718))
-        else:
-            afsluiten = pygame.image.load('Afbeeldingen/afsluiten.png')
-            spelen = pygame.image.load('Afbeeldingen/spelenpressed.png')
-            pygame.time.wait(10000)
 
     elif rules == True:
-        background = pygame.image.load('Afbeeldingen/regelscherm.jpg')
+        background = pygame.image.load('Afbeeldingen/spelregelsbg.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
+        if terug == True:
+            terug = False
+            rules = False
+            menu = True
+
+
 
     else:
-        background = pygame.image.load('Afbeeldingen/playercount.png')
+        background = pygame.image.load('Afbeeldingen/hvlspelers2.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
-
-
-
-
-
-
-
 
     pygame.display.update()
