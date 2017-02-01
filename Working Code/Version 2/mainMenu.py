9798e4f70b263b8ac7ee945fa2c3afb84c52257f
0896320 <0896320@hr.nl>
2017-02-01 13:55:54 +0100

    menu

--- Working Code/Version 2/mainMenu.py
+++ Working Code/Version 2/mainMenu.py
@@ -31,15 +31,21 @@ clicked = False
 rules = False
 terug = False
 terug2 = False
+terug3 = False
 highscore = False
 instellingen = False
+moeilijk = False
+makkelijk = False
+normaal = False
+MusicOff= False
+MusicOn = False
+timer = 50
 MenuMusic = pygame.mixer.Sound("Geluiden/menu.wav")
 
 
 while True:
     mouse = pygame.mouse.get_pos()
-    print(mouse)
-
+    # print(mouse)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -57,21 +63,33 @@ while True:
             terug = True
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1670 and mouse[0] < 1910 and mouse[1] > 980 and mouse[1] < 1060 and highscore == True:
             terug2 = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1140 and mouse[0] < 1390 and mouse[1] > 380 and mouse[1] < 550 and instellingen == True:
+            terug3 = True
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1516 and mouse[0] < 1920 and mouse[1] > 0 and mouse[1] < 150 and menu == True:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 0 and mouse[0] < 400 and mouse[1] > 350 and mouse[1] < 450 and menu == True:
             instellingen = True
             menu = False
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1160 and mouse[0] < 1420 and mouse[1] > 140 and mouse[1] < 240 and instellingen == True:
+            moeilijk = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 380 and mouse[0] < 650 and mouse[1] > 140 and mouse[1] < 240 and instellingen == True:
+            makkelijk = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 740 and mouse[0] < 1000 and mouse[1] > 140 and mouse[1] < 240 and instellingen == True:
+            normaal = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 875 and mouse[0] < 955 and mouse[1] > 445 and mouse[1] < 500 and instellingen == True:
+            MusicOn = True
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 740 and mouse[0] < 820 and mouse[1] > 445 and mouse[1] < 500 and instellingen == True:
+            MusicOff = True
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 175 and mouse[0] < 540 and mouse[1] > 700 and mouse[1] < 830 and menu == False:
             MenuMusic.stop()
-            program(2)
+            program(2, timer)
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 770 and mouse[0] < 1130 and mouse[1] > 700 and mouse[1] < 830 and menu == False:
             MenuMusic.stop()
-            program(3)
+            program(3, timer)
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and mouse[0] > 1360 and mouse[0] < 1770 and mouse[1] > 700 and mouse[1] < 830 and menu == False:
             MenuMusic.stop()
-            program(4)
+            program(4, timer)
 
 
     k = pygame.key.get_pressed()
@@ -79,12 +97,10 @@ while True:
         pygame.quit()
         sys.exit()
     if menu == True:
-        MenuMusic.play()
         background = pygame.image.load('Afbeeldingen/euromastspelbgwtext.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
     elif rules == True:
-        MenuMusic.play()
         background = pygame.image.load('Afbeeldingen/spelregelsbg.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
@@ -93,7 +109,6 @@ while True:
            rules = False
            menu = True
     elif highscore == True:
-        MenuMusic.play()
         background = pygame.image.load('Afbeeldingen/highscoresbg.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
@@ -103,14 +118,28 @@ while True:
            highscore = False
            menu = True
     elif instellingen == True:
-        MenuMusic.play()
         background = pygame.image.load('Afbeeldingen/instellingen.png')
         background = pygame.transform.scale(background, (1920, 1080))
         screen.blit(background, (0, 0))
-        if terug2 == True:
-           terug2 = False
+        if normaal == True:
+            timer = 50
+            normaal = False
+        elif moeilijk == True:
+            timer = 30
+            moeilijk = False
+        elif makkelijk == True:
+            makkelijk = 80
+            makkelijk = False
+        elif terug3 == True:
+           terug3 = False
            instellingen = False
            menu = True
+        elif MusicOn == True:
+            MenuMusic.play()
+            MusicOn = False
+        elif MusicOff == True:
+            MenuMusic.stop()
+            MusicOff = False
     else:
         background = pygame.image.load('Afbeeldingen/hvlspelers2.png')
         background = pygame.transform.scale(background, (1920, 1080))
